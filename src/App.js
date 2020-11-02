361bbf42b27fd559a45df862bf0077d24ba0551d
Dawood Shahid <dawoodshahid67@gmail.com>
2020-11-02 18:19:05 +0500

    test commit

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 function App() {
   return (
     <div>
-    <h1 style={{margin:'50px 0 0 0'}}>New Commit</h1>
+    <h1 style={{margin:'50px 0 0 0'}}>Commit Update from Dawood</h1>
       <Layout>
         <Switch>
           <Route path='/checkout' component={ Checkout } />
